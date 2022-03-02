0b99394599baa5835b97f41d827ee64e2195fd2d
Manikam <manicchart@gmail.com>
2022-03-02 12:39:26 +0530

    pdf change

---
+++ pdf.py
@@ -0,0 +1,40 @@
+
+import PyPDF2
+
+# Creating a pdf file object
+pdfFileObj = open('/home/manikam/Downloads/pm0278_01.pdf','rb')
+
+# Creating a pdf reader object
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+
+# Getting number of pages in pdf file
+pages = pdfReader.numPages
+
+# Loop for reading all the Pages
+for i in range(pages):
+
+        # Creating a page object
+        pageObj = pdfReader.getPage(i)
+
+        # Printing Page Number
+        print("Page No: ",i)
+
+        # Extracting text from page
+        # And splitting it into chunks of lines
+        text = pageObj.extractText().split("  ")
+
+        # Finally the lines are stored into list
+        # For iterating over list a loop is used
+        with open ("/home/manikam/text2.txt","a")as file1:
+            for i in range(len(text)):
+
+                    # Printing the line
+                    # Lines are seprated using "\n"
+                    # print(text[i],end="\n\n")
+                    file1.write(text[i]+"\n\n")
+
+        # For Seprating the Pages
+        print()
+
+# closing the pdf file object
+pdfFileObj.close()
